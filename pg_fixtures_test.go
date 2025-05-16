b46e6910840ee69029b97824c1e865321142189a
Roman Chudov <roman.chudov@gmail.com>
2025-05-16 17:32:41 +0300

    Use testcontainers in integration tests.

--- pg_fixtures_test.go
+++ pg_fixtures_test.go
@@ -11,6 +11,9 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 type user struct {
@@ -40,9 +43,33 @@ type orderProduct struct {
 }
 
 func TestLoad(t *testing.T) {
+	ctx := context.Background()
+
+	postgresContainer, err := postgres.Run(ctx,
+		"postgres:16",
+		postgres.WithDatabase("db"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("password"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second),
+		),
+	)
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		if err := postgresContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	connStr, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
+	require.NoError(t, err)
+
 	cfg := &Config{
 		FilePath: "testdata/fixtures_01.yml",
-		ConnStr:  "postgres://user:password@localhost:5432/db?sslmode=disable",
+		ConnStr:  connStr,
 		Truncate: true,
 		ResetSeq: true,
 		DryRun:   false,
@@ -88,7 +115,7 @@ func TestLoad(t *testing.T) {
 	require.Equal(t, "User2", users[1].Name)
 	require.True(t, users[1].LastLoginAt.After(users[1].CreatedAt))
 
-	// check orders orders
+	// check orders
 	rows, err = db.Query("SELECT id, user_id, created_at FROM orders ORDER BY id")
 	require.NoError(t, err)
 	defer rows.Close()
